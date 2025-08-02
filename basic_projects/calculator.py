881e623cf7cabefa9787d1f66ccaadc98c738a10
Gyan Prakash <gyansir22@gmail.com>
2025-08-02 23:22:54 +0530

    Create calculator.py
    Basic Calculator in Python 🧮
    
    This is a simple Python-based calculator that performs basic arithmetic operations including:
    
    Addition
    
    Subtraction
    
    Multiplication
    
    Division
    
    The calculator takes two numbers and an operator as input from the user. It uses if-elif-else statements to determine the operation to be performed.
    
    All results involving floating-point values are rounded off to 3 decimal places for cleaner and more precise output.

---
+++ basic_projects/calculator.py
@@ -0,0 +1,22 @@
+#calculator
+
+operator = input("Enter your operator: ")
+num1 = float(input("enter the first number: "))
+num2 = float(input("enter the second number: "))
+
+if operator == "+":
+    result = num1+num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1-num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1*num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1/num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid operator")
+
+
