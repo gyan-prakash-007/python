04f9bf5ffe4ff02426a68a5ea061873b7f4fe424
Gyan Prakash <gyansir22@gmail.com>
2025-08-09 21:16:52 +0530

    shopping cart program.py
    🛒 Simple Shopping Cart Program (Python)
    
    This is a beginner-friendly Python program that simulates a basic shopping cart.
    It allows the user to:
    
    Add items (food names) to the cart.
    
    Enter prices for each item.
    
    Quit by typing q.
    
    Display the cart contents and the total cost at the end.
    
    💡 How it works:
    
    The program repeatedly asks the user for an item to purchase.
    
    If the user enters q, the program stops asking for more items.
    
    It then prints all the items in the cart and calculates the total bill.

---
+++ basic_projects/shopping cart program.py
@@ -0,0 +1,11 @@
+C:\Users\DELL\PycharmProjects\JupyterProject\.venv\Scripts\python.exe "C:\Users\DELL\PycharmProjects\JupyterProject\mini projects\shopping cart.py" 
+Enter a food to buy (q to quit) : rice 
+ Enter the price of a rice : $2
+Enter a food to buy (q to quit) : wheat 
+ Enter the price of a wheat : $5 
+Enter a food to buy (q to quit) : q
+_____YOUR CART_____
+rice  wheat  
+Your total is: $7.0
+
+Process finished with exit code 0
